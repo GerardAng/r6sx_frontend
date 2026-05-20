e70f453c2dd70b07ea021c319342fd0bfc5fa4c9
GerardAng <gerardangarita@gmail.com>
2026-05-19 22:16:23 -0500

    fix imagen personaje

--- src/components/DetallePersonaje.jsx
+++ src/components/DetallePersonaje.jsx
@@ -12,7 +12,7 @@ const DetallePersonaje = ({ personaje }) => {
             <h2>{personaje.nombre_personaje}</h2>
             {personaje.imagen ? (
                 <img
-                    src={`http://127.0.0.1:8000${personaje.imagen}`}
+                    src={personaje.imagen}
                     alt={personaje.nombre_personaje}
                     className="detalle-imagen"
                 />
